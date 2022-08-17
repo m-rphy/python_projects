775363d0e45470d59d100b1bf3b71be0c46fdfd8
olsoninoslo <107144841+olsoninoslo@users.noreply.github.com>
2022-08-17 16:34:31 -0600

    Add files via upload
    The functions in this file perform the basic algorithms of ML, namely logistic and linear regression. There are functions for vectorized data as well as functions that perform the algorithm via iterative methods.

---
+++ ML_tools.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python3
+
+
+# This is a list of machine learning tools to be used on various datasets. 
+# There are function that utilize iterative loops as well as vectorized 
+# functions for increase performance. 
+
+import numpy as np
+
+# Loop version of multi-variable compute_cost
+def compute_cost(X, y, w, b):
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+    Returns
+      cost (scalar)    : cost
+    """
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        f_wb_i = np.dot(X[i],w) + b           #(n,)(n,)=scalar
+        cost = cost + (f_wb_i - y[i])**2
+    cost = cost/(2*m)
+    return cost 
+
+    #Function to calculate the cost
+
+def compute_lin_cost_matrix(X, y, w, b, verbose=False):
+    """
+    Computes the gradient for linear regression
+     Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      verbose : (Boolean) If true, print out intermediate value f_wb
+    Returns
+      cost: (scalar)
+    """
+    m = X.shape[0]
+
+    # calculate f_wb for all examples.
+    f_wb = X @ w + b
+    # calculate cost
+    total_cost = (1/(2*m)) * np.sum((f_wb-y)**2)
+
+    if verbose: print("f_wb:")
+    if verbose: print(f_wb)
+
+    return total_cost
+
+def compute_logistic_cost_matrix(X, y, w, b, logistic=False, lambda_=0, safe=True):
+    """
+    Computes the cost using  using matrices
+    Args:
+      X : (ndarray, Shape (m,n))          matrix of examples
+      y : (ndarray  Shape (m,) or (m,1))  target value of each example
+      w : (ndarray  Shape (n,) or (n,1))  Values of parameter(s) of the model
+      b : (scalar )                       Values of parameter of the model
+      verbose : (Boolean) If true, print out intermediate value f_wb
+    Returns:
+      total_cost: (scalar)                cost
+    """
+    m = X.shape[0]
+    y = y.reshape(-1,1)             # ensure 2D
+    w = w.reshape(-1,1)             # ensure 2D
+    if logistic:
+        if safe:  #safe from overflow
+            z = X @ w + b                                                           #(m,n)(n,1)=(m,1)
+            cost = -(y * z) + log_1pexp(z)
+            cost = np.sum(cost)/m                                                   # (scalar)
+        else:
+            f    = sigmoid(X @ w + b)                                               # (m,n)(n,1) = (m,1)
+            cost = (1/m)*(np.dot(-y.T, np.log(f)) - np.dot((1-y).T, np.log(1-f)))   # (1,m)(m,1) = (1,1)
+            cost = cost[0,0]                                                        # scalar
+    else:
+        f    = X @ w + b                                                        # (m,n)(n,1) = (m,1)
+        cost = (1/(2*m)) * np.sum((f - y)**2)                                   # scalar
+
+    reg_cost = (lambda_/(2*m)) * np.sum(w**2)                                   # scalar
+
+    total_cost = cost + reg_cost                                                # scalar
+
+    return total_cost                                                           # scalar
+
+
+
+    def compute_cost_logistic(X, y, w, b, lambda_=0, safe=False):
+    """
+    Computes cost using logistic loss, non-matrix version
+
+    Args:
+      X (ndarray): Shape (m,n)  matrix of examples with n features
+      y (ndarray): Shape (m,)   target values
+      w (ndarray): Shape (n,)   parameters for prediction
+      b (scalar):               parameter  for prediction
+      lambda_ : (scalar, float) Controls amount of regularization, 0 = no regularization
+      safe : (boolean)          True-selects under/overflow safe algorithm
+    Returns:
+      cost (scalar): cost
+    """
+
+    m,n = X.shape
+    cost = 0.0
+    for i in range(m):
+        z_i    = np.dot(X[i],w) + b                                             #(n,)(n,) or (n,) ()
+        if safe:  #avoids overflows
+            cost += -(y[i] * z_i ) + log_1pexp(z_i)
+        else:
+            f_wb_i = sigmoid(z_i)                                                   #(n,)
+            cost  += -y[i] * np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i)       # scalar
+    cost = cost/m
+
+    reg_cost = 0
+    if lambda_ != 0:
+        for j in range(n):
+            reg_cost += (w[j]**2)                                               # scalar
+        reg_cost = (lambda_/(2*m))*reg_cost
+
+    return cost + reg_cost                                                          # scalar
+
+
+
+def compute_gradient(X, y, w, b):
+    """
+    Computes the gradient for linear regression
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+    Returns
+      dj_dw (ndarray Shape (n,)): The gradient of the cost w.r.t. the parameters w.
+      dj_db (scalar):             The gradient of the cost w.r.t. the parameter b.
+    """
+    m,n = X.shape           #(number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):
+        err = (np.dot(X[i], w) + b) - y[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err * X[i,j]
+        dj_db = dj_db + err
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+
+    return dj_db,
+
+def compute_gradient_logistic(X, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+    Returns
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                           #(n,)
+    dj_db = 0.
+
+    for i in range(m):
+        x_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(x_i)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+        
+    return dj_db, dj_dw  
+
+    def compute_gradient_matrix(X, y, w, b):
+    """
+    Computes the gradient for linear regression
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+    Returns
+      dj_dw (ndarray (n,1)): The gradient of the cost w.r.t. the parameters w.
+      dj_db (scalar):        The gradient of the cost w.r.t. the parameter b.
+
+    """
+    m,n = X.shape
+    f_wb = X @ w + b
+    e   = f_wb - y
+    dj_dw  = (1/m) * (X.T @ e)
+    dj_db  = (1/m) * np.sum(e)
+
+    return dj_db,dj_dw
+
+    def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    '''
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      X : (array_like Shape (m,n)    matrix of examples 
+      y : (array_like Shape (m,))    target value of each example
+      w_in : (array_like Shape (n,)) Initial values of parameters of the model
+      b_in : (scalar)                Initial value of parameter of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (array_like Shape (n,)) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    '''
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store values at each iteration primarily for graphing later
+    hist={}
+    hist["cost"] = []; hist["params"] = []; hist["grads"]=[]; hist["iter"]=[];
+    
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    save_interval = np.ceil(num_iters/10000) # prevent resource exhaustion for long runs
+
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db,dj_dw = gradient_function(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J,w,b at each save interval for graphing
+        if i == 0 or i % save_interval == 0:     
+            hist["cost"].append(cost_function(X, y, w, b))
+            hist["params"].append([w,b])
+            hist["grads"].append([dj_dw,dj_db])
+            hist["iter"].append(i)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            #print(f"Iteration {i:4d}: Cost {cost_function(X, y, w, b):8.2f}   ")
+            cst = cost_function(X, y, w, b)
+            print(f"Iteration {i:9d}, Cost: {cst:0.5e}")
+    return w, b, hist #return w,b and history for graphing
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters): 
+    '''
+    Performs batch gradient descent
+    
+    Args:
+      X (ndarray (m,n)   : Data, m examples with n features
+      y (ndarray (m,))   : target values
+      w_in (ndarray (n,)): Initial values of model parameters  
+      b_in (scalar)      : Initial values of model parameter
+      alpha (float)      : Learning rate
+      num_iters (scalar) : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,))   : Updated values of parameters
+      b (scalar)         : Updated value of parameter 
+    '''
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append(compute_cost_logistic(X, y, w, b) )
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+        
+    return w, b, J_history         #return final w,b and J history for graphing
+
+
+
+
+    def zscore_normalize_features(X,rtn_ms=False):
+    '''
+    returns z-score normalized X by column
+    Args:
+      X : (numpy array (m,n)) 
+    Returns
+      X_norm: (numpy array (m,n)) input normalized by column
+    '''
+    mu     = np.mean(X,axis=0)  
+    sigma  = np.std(X,axis=0)
+    X_norm = (X - mu)/sigma      
+
+    if rtn_ms:
+        return(X_norm, mu, sigma)
+    else:
+        return(X_norm)
+    
+    def log_1pexp(x, maximum=20):
+    ''' approximate log(1+exp^x)
+        https://stats.stackexchange.com/questions/475589/numerical-computation-of-cross-entropy-in-practice
+    Args:
+    x   : (ndarray Shape (n,1) or (n,)  input
+    out : (ndarray Shape matches x      output ~= np.log(1+exp(x))
+    '''
+
+    out  = np.zeros_like(x,dtype=float)
+    i    = x <= maximum
+    ni   = np.logical_not(i)
+
+    out[i]  = np.log(1 + np.exp(x[i]))
+    out[ni] = x[ni]
+    return out
+
+    def sigmoid(x):
+     return 1/(1 + np.exp(-x))
